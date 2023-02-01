563905d8855ba562d1861f9768a0910bb25bd02e
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-01 12:14:03 +0630

    First Commit, Some design add button

---
+++ src/components/Block.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="block">
+    <p>Click Here {{delay}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['delay']
+}
+</script>
+
+<style>
+ .block{
+    background-color: aqua;
+    width: 400px;
+    height: 300px;
+    padding-top: 30px;
+    margin: 20px auto;
+ }
+</style>
